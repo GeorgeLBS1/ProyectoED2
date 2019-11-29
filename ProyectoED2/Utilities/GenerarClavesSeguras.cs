1ce0fa54af1cb3fc289ddc93c2aa7c7530d6d962
José Meléndez <josemel1103@gmail.com>
2019-11-29 11:39:28 -0600

    Implementacion algoritmo Diffie-Helman

---
+++ ProyectoED2/Utilities/GenerarClavesSeguras.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace ProyectoED2.Utilities
+{
+    public class GenerarClavesSeguras
+    {
+        public int GenerarLlave(int miLlave, int LlaveEmisor)
+        {
+            int LlavePublica = Llave(LlaveEmisor);
+            int LlaveComun = DiffieHelman(miLlave, LlavePublica);
+            return LlaveComun;
+        }
+        int Llave(int LlaveInsegura)
+        {
+            BigInteger p = 17;
+            BigInteger g = 989;
+            BigInteger Llave = new BigInteger(LlaveInsegura);
+            int Resultado = (int)BigInteger.ModPow(g, Llave, p);
+            return Resultado;
+        }
+
+        int DiffieHelman(int LlaveInseguraPropia, int LlavePublica)
+        {
+            BigInteger P = 17;
+            BigInteger LlaveInsegura = new BigInteger(LlaveInseguraPropia);
+            BigInteger LlaveP = new BigInteger(LlavePublica);
+            int K = (int)BigInteger.ModPow(LlaveP, LlaveInsegura, P);
+            return K;
+
+        }
+    }
+}
